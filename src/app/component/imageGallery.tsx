de31627bb7fd3b77a76ef0a2bf651df9996355ad
saadia <saadia.52004@gmail.com>
2024-11-07 22:07:13 +0500

    Image Gallery for indivisual Product

--- src/app/component/imageGallery.tsx
+++ src/app/component/imageGallery.tsx
@@ -11,6 +11,7 @@ export default function ImageGallery({ image }: iAppProp) {
   const handleImgae = (images: any) => setbigimgae(images);
   return (
     <div className="container mt-2 ">
+      {/* Main Big Image */}
       <div className="relative overflow-hidden rounded-lg">
         <Image
           src={urlFor(bigimage).url()}
@@ -19,11 +20,12 @@ export default function ImageGallery({ image }: iAppProp) {
           height={500}
           className=" object-cover object-center"
         />
+        {/* sales tag */}
         <span className="absolute left-0 top-0 rounded-br-lg bg-red-800 px-3 py-1.5 uppercase tracking-wider text-body">
           Sales
         </span>
       </div>
-
+      {/* Bottom small images */}
       <div className="order-last grid grid-cols-3 lg:grid-cols-4 sm:grid-cols-4 mt-4 gap-3 lg:gap-2 ">
         {image.map((images: any, idx: any) => (
           <div key={idx} className="rounded-lg">
