d23a1ae528af448b8f279faa8b99c209c877bfd5
saadia <saadia.52004@gmail.com>
2024-11-07 22:18:26 +0500

    New Arrivals

--- src/app/component/NewArrivals.tsx
+++ src/app/component/NewArrivals.tsx
@@ -32,13 +32,11 @@ const NewArrivals = async () => {
   return (
     <div className="container mt-16 ">
       {/* name */}
-
       <div className="container px-5 mx-auto ">
         <div className="text-center">
           <h1 className="text-4xl xl:text-4xl lg:text-4xl sm:text-5xl title-font  text-gray-900 font-extrabold capitalize ">
             New Arrivals
           </h1>
-
           <div className="flex mt-2 justify-center">
             <div className="w-20 h-1 rounded-full bg-mypurple inline-flex" />
           </div>
@@ -49,6 +47,7 @@ const NewArrivals = async () => {
       <div className="container grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 sm:grid-cols-2 place-items-center sm:gap-10 gap-6">
         {data.map((item) => (
           <div key={item._id} className="mt-7 rounded-lg ">
+            
             {/* product Image */}
             <Link href={`/product/${item.slug}`}>
               <div className="  hover:border border-gray-400 duration-100">
@@ -67,25 +66,17 @@ const NewArrivals = async () => {
             </Link>
 
             {/* price */}
-            
-              <div className="lg:flex xl:flex sm:flex gap-2 mt-1">
-                <span className=" text-[#d00000] line-through  ">
-                  {`PKR ${item.prevoiusprice}`}
-                </span>
-                <p className=" text-mytext font-bold text-xl">{`PKR ${item.price}.00`}</p>
-              </div>
-          
-            {/* butoon for view details */}
-            {/* <Link href={`/product/${item.slug}`}>
-              <button className="bg-mypink text-center p-2 rounded-full font-medium text-xl mt-4 hover:bg-mytext duration-500 text-body w-full">
-                View More Details
-              </button>
-            </Link> */}
+            <div className="lg:flex xl:flex sm:flex gap-2 mt-1">
+              <span className=" text-[#d00000] line-through  ">
+                {`PKR ${item.prevoiusprice}`}
+              </span>
+              <p className=" text-mytext font-bold text-xl">{`PKR ${item.price}.00`}</p>
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default NewArrivals;
