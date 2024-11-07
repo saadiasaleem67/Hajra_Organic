a39d49fa309931f3fcd48fa84f42bc2d7b89d667
saadia <saadia.52004@gmail.com>
2024-11-07 22:01:58 +0500

    Deals Page

--- src/app/component/Deals.tsx
+++ src/app/component/Deals.tsx
@@ -19,7 +19,7 @@ const getdata = async () => {
    _id,
    "slug":slug.current,
    "imageUrl":image[0].asset->url,
- prevoiusprice,
+   prevoiusprice,
    "category":category[]->{
      name
    }
@@ -32,19 +32,17 @@ const Deals = async () => {
   return (
     <div className="container mt-16">
       {/* name */}
-
       <div className="container px-5 mx-auto ">
         <div className="text-center">
           <h1 className="text-4xl xl:text-4xl lg:text-4xl sm:text-5xl title-font  text-gray-900 font-extrabold capitalize ">
             Bundles & Deals
           </h1>
-
           <div className="flex mt-2 justify-center">
             <div className="w-20 h-1 rounded-full bg-mypurple inline-flex" />
           </div>
         </div>
       </div>
-
+      {/* Product Data */}
       <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 place-items-center sm:gap-10 lg:gap-10  ">
         {data.map((item) => (
           <div key={item._id} className="mt-7 rounded-lg ">
@@ -60,27 +58,19 @@ const Deals = async () => {
                 />
               </div>
 
-              {/* product data */}
               <div className="mt-2">
+                {/* product Name */}
                 <p className=" text-mytext font-bold "> {item.name}</p>
               </div>
             </Link>
 
             {/* price */}
-
             <div className="lg:flex xl:flex sm:flex gap-3 mt-1">
               <span className=" text-[#d00000] line-through   ">
                 {`PKR ${item.prevoiusprice}`}
               </span>
               <p className=" text-mytext font-bold text-xl">{`PKR ${item.price}.00`}</p>
             </div>
-
-            {/* butoon for view details */}
-            {/* <Link href={`/productdeal/${item.slug}`}>
-              <button className="bg-mypink text-center p-2 rounded-full font-medium text-xl mt-2 hover:bg-mytext duration-500 text-body w-full ">
-                View More Details
-              </button>
-            </Link> */}
           </div>
         ))}
       </div>
