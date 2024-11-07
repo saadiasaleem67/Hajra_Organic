ccb297e2620666a8ac00d44b2850332797620a05
saadia <saadia.52004@gmail.com>
2024-11-07 21:15:11 +0500

    AddtoBag

--- src/app/component/addtobag.tsx
+++ src/app/component/addtobag.tsx
@@ -3,30 +3,29 @@ import { urlFor } from "@/sanity/lib/image";
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
 
-
 export interface ProductCart {
   name: string;
   price: number;
   currency: string;
   image: any;
-  id:string
+  id: string;
 }
 
-const Addtobag = ({ name, price, currency, image ,id}: ProductCart) => {
-  const { addItem,handleCartClick} = useShoppingCart();
+const Addtobag = ({ name, price, currency, image, id }: ProductCart) => {
 
-
+  const { addItem, handleCartClick } = useShoppingCart();
   const product = {
     name: name,
     price: price,
     currency: currency,
-    image: urlFor(image).url() ,
+    image: urlFor(image).url(),
     id: id,
   };
   return (
     <button
-    className="bg-pink-900 text-center p-2  font-medium text-xl hover:bg-mytext duration-500 text-body w-full" onClick={() => {
-        addItem(product),handleCartClick()
+      className="bg-pink-900 text-center p-2  font-medium text-xl hover:bg-mytext duration-500 text-body w-full"
+      onClick={() => {
+        addItem(product), handleCartClick();
       }}
     >
       Add To Cart
